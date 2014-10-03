75b4c68c77d57db65ef43c9ebcf331db19a1a8fc
Victor Diniz <blek.net@gmail.com>
2014-10-03 14:29:56 -0300

    Main.c atualizado com a versão nova fornecida

--- tp1/src/main.c
+++ tp1/src/main.c
@@ -5,38 +5,40 @@
 
 #define MAXLINE 256 // tamanho do buffer da linha
 #define SEP " \n"   // separador de instrucoes
-#define SEPVAR "V" // separador do id da variavel
+#define SEPVAR  "V" // separador do id da variavel
 
 // instrucoes
-#define MEM "MEM"
-#define VAR "VAR"
-#define VALLOC "VALLOC"
-#define VCALLOC "VCALLOC"
-#define VREALLOC "VREALLOC"
-#define VFREE "VFREE"
-#define PRINT "PRINT"
-
+#define MEM     "MEM"
+#define VAR     "VAR"
+#define VALLOC      "VALLOC"
+#define VCALLOC     "VCALLOC"
+#define VREALLOC    "VREALLOC"
+#define VFREE       "VFREE"
+#define PRINT       "PRINT"
 
 int main(int argc, char * argv[])
 {
-    FILE *pIn = NULL; // Arquivo de entrada
+    FILE    *pIn    = NULL; // Arquivo de entrada
 
-    char *token = NULL, // token de instrucao
-          line[MAXLINE]; // buffer de linha
+    char    *token = NULL,  // token de instrucao
+        line[MAXLINE];  // buffer de linha
 
-    short int **Var; // vetor de variaveis alocadas com valloc
+    char    **Var;      // vetor de variaveis alocadas com valloc
 
-    int nVars=0, // numero de variaveis a serem criadas
-        id=0; // identificador de variavel
+    int nVars=0,    // numero de variaveis a serem criadas
+        id=0,       // identificador de variavel
+        allZero=0;  // flag de verificacao do vcalloc
     
-    size_t tamBloco=0; // tamanho do bloco a ser alocado
-    size_t numBlocos=0; // (corrigido manualmente, número de blocos p/ vcalloc)
+    size_t  tamBloco=0, // tamanho do bloco a ser alocado
+        nItens=0,   // numero de itens alocados pelo vcalloc
+        tamItem,    // tamanho de cada item alocado pelo vcalloc
+        i=0;
 
 
     // Verifica o numero de argumentos
     if( argc < 2 )
     {
-        printf("Utilização: ./tp1 <entrada>");
+        printf("Utilização: ./tp1 <entrada> <saida>");
         exit(EXIT_FAILURE);
     }
 
@@ -54,29 +56,30 @@ int main(int argc, char * argv[])
     inicializa_gerencia();
 
     // executa todos os comandos do arquivo de entrada
-    while(1){
+    while( 1 ){
 
         // linha por linha
-        fgets(line, MAXLINE, pIn);
-
+        fgets(line, MAXLINE, pIn);  // tamanho do bloco
+        
         // verifica se encontrou um final de arquivo
-        if (feof(pIn))break;
+        if( feof(pIn) )break;
 
         // obtem comando
         token = strtok(line, SEP);
 
+
         // reconhece os comandos
         
         // VAR: Define numero de variaveis
-        if (strcmp(VAR, token) == 0){
+        if ( strcmp(VAR, token) == 0 ){
             token = strtok(NULL, SEP);
             nVars = atoi(token);
-            Var = (short int**)malloc(sizeof(short int*)*nVars);
+            Var = (char **)malloc(sizeof(char*)*nVars);
             printf("VAR %d\n",nVars);
         }
 
         // VALLOC: Aloca espaco da memoria
-        if (strcmp(VALLOC,token) == 0){
+        if ( strcmp(VALLOC,token) == 0 ){
             // tamanho do bloco
             token = strtok(NULL, SEP);
             tamBloco = atoi(token);
@@ -87,21 +90,21 @@ int main(int argc, char * argv[])
             id = atoi(token);
             
             // aloca espaco e armazena no vetor
-            Var[id] = (short int*)valloc(tamBloco);
+            Var[id] = (char*)valloc(tamBloco);
             printf("\n");
-            if(Var[id]!=NULL)printf("VALLOC %zu V%d\tADDR %p\n", tamBloco, id, Var[id]);
-            else printf("VALLOC %zu V%d\tADDR NULL\n",tamBloco, id);
+            if(Var[id]!=NULL)printf("VALLOC %zu V%d\tOK\n",tamBloco, id);
+            else printf("VALLOC %zu V%d\tNULL\n",tamBloco, id);
         }
         
         // VCALLOC: aloca espaco da memoria e preenche com zeros
-        if (strcmp(VCALLOC, token) == 0){
-            // número de blocos
+        if ( strcmp(VCALLOC, token) == 0 ){
+            // numero de itens alocados
             token = strtok(NULL, SEP);
-            numBlocos = atoi(token);
+            nItens = atoi(token);
 
             // tamanho do bloco
             token = strtok(NULL, SEP);
-            tamBloco = atoi(token);
+            tamItem = atoi(token);
 
             // id da variavel
             token = strtok(NULL, SEP);  // V[id]
@@ -109,14 +112,20 @@ int main(int argc, char * argv[])
             id = atoi(token);
             
             // aloca espaco e armazena no vetor
-            Var[id] = (short int*)vcalloc(numBlocos, tamBloco);
+            Var[id] = (char*)vcalloc(nItens, tamItem);
             printf("\n");
-            if(Var[id]!=NULL)printf("VCALLOC %zu %zu V%d\tADDR %p\n",numBlocos, tamBloco, id, Var[id]);
-            else printf("VCALLOC %zu V%d\tADDR NULL\n",tamBloco, id);
+            if(Var[id]!=NULL){
+                allZero = 0;
+                for(i=0;i<nItens*(tamItem/sizeof(char));i++){
+                    if(Var[id][i]!=0)allZero = 1;
+                }
+                printf("VCALLOC %zu V%d\t%s\n",tamBloco, id, allZero==0 ? "OK" : "NOT ZERO");
+            }
+            else printf("VCALLOC %zu V%d\tNULL\n",tamBloco, id);
         }
 
         // VREALLOC: realoca espaco da memoria
-        if (strcmp(VREALLOC, token) == 0){
+        if ( strcmp(VREALLOC, token) == 0 ){
             // tamanho do bloco
             token = strtok(NULL, SEP);
             tamBloco = atoi(token);
@@ -126,14 +135,14 @@ int main(int argc, char * argv[])
             token = strtok(token, SEPVAR);  // id
             id = atoi(token);
             
-            Var[id] = (short int*)vrealloc(Var[id], tamBloco);
+            Var[id] = (char*)vrealloc(Var[id], tamBloco);
             printf("\n");
-            if (Var[id]!=NULL)printf("VREALLOC %zu V%d\tADDR %p\n",tamBloco, id, Var[id]);
-            else printf("VREALLOC %zu V%d\tADDR NULL\n",tamBloco, id);
+            if(Var[id]!=NULL)printf("VREALLOC %zu V%d\tOK\n",tamBloco, id);
+            else printf("VREALLOC %zu V%d\tNULL\n",tamBloco, id);
         }
 
         // VFREE: libera espacos de memoria
-        if (strcmp(VFREE, token) == 0){
+        if ( strcmp(VFREE, token) == 0 ){
             // id da variavel
             token = strtok(NULL, SEP);  // V[id]
             token = strtok(token, SEPVAR);  // id
@@ -144,7 +153,7 @@ int main(int argc, char * argv[])
             printf("VFREE %d\n", id);
         }
         // PRINT: Imprime o estado da memoria
-        if (strcmp(PRINT, token) == 0){
+        if ( strcmp(PRINT, token) == 0){
             imprime_status_memoria();
         }
     }
